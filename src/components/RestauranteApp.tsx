fb96bac4701d7a1622aec645b923df2161dac6c1
yanpiccolo-ship-it <yanpiccolo@gmail.com>
2026-04-10 23:20:14 +0200

    Create RestauranteApp.tsx

---
+++ src/components/RestauranteApp.tsx
@@ -0,0 +1,12 @@
+// Restaurant Application
+import React from 'react';
+
+const RestauranteApp: React.FC = () => {
+    return (
+        <div>
+            <h1>Restaurante Application</h1>
+        </div>
+    );
+};
+
+export default RestauranteApp;
